997c5219a7f6623ae1ccae007eee6fd7f46ae228
Your Name <you@example.com>
2022-03-18 22:17:10 -0300

    new try

--- list.c
+++ list.c
@@ -45,7 +45,7 @@ void * firstList(List * list) {
 
 void * nextList(List * list) {
     if(list->head == NULL) return NULL;
-    if(list->current== lastList(list)) return NULL;
+    if(list->current == lastList(list)) return NULL;
     if(list->current->next != NULL && list->current->next->data != NULL){
         list->current = list->current->next;
         return list->current->data;
@@ -58,7 +58,7 @@ void * lastList(List * list) {
     while(aux != NULL){
         aux = aux->next;
     }
-    list->current = aux;
+    list->tail = aux;
     return aux;
 }
 
